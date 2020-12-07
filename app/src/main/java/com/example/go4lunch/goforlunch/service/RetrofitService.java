2a8b17b752ade61ee5acdf0ce416238a1b8051c6
Ginjiii <61824233+Ginjiii@users.noreply.github.com>
2020-12-07 11:14:50 +0100

    adding Google places

---
+++ app/src/main/java/com/example/go4lunch/goforlunch/service/RetrofitService.java
@@ -0,0 +1,17 @@
+package com.example.go4lunch.goforlunch.service;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitService {
+
+    public static Retrofit retrofit = new Retrofit.Builder()
+            //define root URL
+            .baseUrl("https://maps.googleapis.com/")
+            //serialize Gson
+            .addConverterFactory(GsonConverterFactory.create())
+            //For RxJava
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build();
+}
